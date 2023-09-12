1a29e68bc25cb805aa5bbe67a4be7f6d38ef8467
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-09-12 11:01:48 +0200

    changed to oackage-template, added correct junit version

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@
 module gr.javafx.gr {
     requires javafx.controls;
     requires javafx.fxml;
-    exports gr2340.javafx.gr2340;
-    opens gr2340.javafx.gr2340 to javafx.fxml;
+    exports gr2340;
+    opens gr2340 to javafx.fxml;
 }
 
