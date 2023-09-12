e6f8721908fd6c1734eccda6bb3affe46743dfdd
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-09-12 11:46:56 +0200

    renamed

--- src/test/java/ui/AppTest.java
+++
@@ -1,8 +0,0 @@
-package ui;
-import org.junit.jupiter.api.*;
-
-
-
-public class AppTest {
-
-}
---
+++ src/test/java/ui/HelloApplicationTest.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloApplicationTest {
+
+}
