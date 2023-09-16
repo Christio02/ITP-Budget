dde102dbe829ddfe8c2c3a3fdc0e943ca58939a4
B1uskyy <mats.kvanvik@hotmail.com>
2023-09-16 17:13:42 +0200

    added check for valid categories

--- src/main/java/core/Calculation.java
+++ src/main/java/core/Calculation.java
@@ -2,21 +2,29 @@ package core;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Calculation {
 
     private Map<String, ArrayList<Integer>> categoriesAndAmount;
+    private ArrayList<String> validCategories = new ArrayList<>(Arrays.asList("Food", "Transportation", "Entertainment", "Clothing", "Other"));
 
     public Calculation() {
         categoriesAndAmount = new HashMap<>();
     }
 
     public void addAmountToCategory(String category, int amount) {
-        if (!checkAmountIscorrect(amount)) {
+        if (!checkAmountIscorrect(amount))  {
             throw new IllegalArgumentException("Cannot add non-positive values!");
         }
+
+        if (!checkValidCategory(category)) {
+            throw new IllegalArgumentException("Invalid category!");
+        }
+
+
             if (categoriesAndAmount.containsKey(category)) {
                 ArrayList<Integer> tempArray = this.categoriesAndAmount.get(category);
                 tempArray.add(amount);
@@ -29,7 +37,9 @@ public class Calculation {
 
     }
 
-
+    private boolean checkValidCategory(String category) {
+        return validCategories.contains(category);
+    }
 
     private boolean checkAmountIscorrect(int amount) {
         return amount >= 0;
