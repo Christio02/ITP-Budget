6415181bd119161306b154d4e17dd7e32847a3d0
Eirik Kvam <eirik.engen.kvam@gmail.com>
2023-09-18 20:59:20 +0200

    Added styling to start-menu + added logo

--- src/main/java/core/Calculation.java
+++ src/main/java/core/Calculation.java
@@ -1,10 +1,6 @@
 package core;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Calculation {
 
--- src/main/java/ui/BudgetApplication.java
+++ src/main/java/ui/BudgetApplication.java
@@ -11,7 +11,7 @@ public class BudgetApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BudgetApplication.class.getResource("startmenu-fxml.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         scene.getStylesheets().add(getClass().getResource("style/style.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
