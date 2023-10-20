da0bfe51128f2447b4c8601763f69983d98957a1
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-10-20 15:03:20 +0200

    fix: tested new hashmap implementation

--- budget/ui/src/main/java/budget/ui/BudgetsViewController.java
+++ budget/ui/src/main/java/budget/ui/BudgetsViewController.java
@@ -1,14 +1,11 @@
 package budget.ui;
 
-import budget.core.Budgets;
 import budget.core.Calculation;
 import budget.core.Category;
 import budget.utility.FileUtility;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
 
@@ -19,63 +16,31 @@ public class BudgetsViewController {
     @FXML
     private ListView<String> budgetsList;
 
-
     @FXML
-    private Button returnMenuBtn;
-
-    private static BudgetsViewController instance;
-    public static BudgetsViewController getInstance() {
-        return instance;
-    }
-
-
-
-    private Budgets budgets;
+    private Text budgetTitle;
 
     private StartMenuController menuController = new StartMenuController();
-    private FileUtility fileUtility;
     @FXML
     public void initialize() {
-        instance = this;
+        Calculation calc1 = new Calculation();
         menuController = StartMenuController.getInstance();
-
         try {
-            if (fileUtility.getLoad()) {
-                budgets = fileUtility.readFile();
-                System.out.println(budgets.retrieveBudgets());
+            if (FileUtility.getLoad()) {
+                FileUtility.readFromFile(calc1);
+                String name = menuController.getCalcName();
+                budgetTitle.setText(name);
             }
+
         } catch (IOException e) {
             e.getStackTrace();
         }
 
+
+
         ObservableList<String> listOfBudgets = FXCollections.observableArrayList();
-
-        if (budgets != null) {
-            for (Calculation calc : budgets.retrieveBudgets()) {
-                String budgetName = calc.getName(); // Get the name of the current budget
-                listOfBudgets.add("Name: " + budgetName);
-                for (Category category : calc.getCategoriesList()) {
-                    listOfBudgets.add("Category: " + category.getCategoryName());
-                    listOfBudgets.add("Total Amount: " + calc.getSum(category));
-                }
-            }
-
-            // Calculate the total sum
-            double totalSum = budgets.retrieveBudgets().stream()
-                    .mapToDouble(calc -> calc.getCategoriesList().stream()
-                            .mapToDouble(calc::getSum)
-                            .sum())
-                    .sum();
-            listOfBudgets.add("Total Sum of All Budgets: " + totalSum);
-        }
 
         budgetsList.setItems(listOfBudgets);
 
-    }
-
-    @FXML
-    private void loadMainMenu(ActionEvent event) throws Exception {
-        ChangeScene.changeToScene(getClass(), event, "startmenu-fxml.fxml");
     }
 
 
