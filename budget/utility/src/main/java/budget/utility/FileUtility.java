ca00e74eaadae028dbcfe4b51794fb1d71be5a79
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-10-20 15:03:43 +0200

    test: tested hashmap implementation

--- budget/utility/src/main/java/budget/utility/FileUtility.java
+++ budget/utility/src/main/java/budget/utility/FileUtility.java
@@ -1,91 +1,89 @@
-package budget.utility;
+    package budget.utility;
 
-import budget.core.Budgets;
-import budget.core.Calculation;
+    import budget.core.Calculation;
+    import budget.core.Category;
+    import com.fasterxml.jackson.core.type.TypeReference;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
-/**
- * This class is responsible for file operations.
- */
-public class FileUtility {
-    /**
-     * Load status.
-     */
-    private boolean load;
+    import java.io.File;
+    import java.io.IOException;
+    import java.util.Map;
 
     /**
-     * Current directory path.
+     * This class is responsible for file operations.
      */
-    private static final String CURRENT_DIR = System.getProperty("user.dir");
+    public final class FileUtility {
 
-    /**
-     * File path for serialization.
-     */
-    private static final String FILE_PATH = CURRENT_DIR
-            + "/../utility/src/main/resources/budget/utility/savedBudget.json";
+        /**
+         * Load status.
+         */
+        private static boolean load;
 
-    private static final String testFilePath = "C:/Users/chris/Documents/Coding - NTNU/ITP/gr2340/budget/utility/src/main/resources/budget/utility/savedBudget.json";
-    /**
-     * Private constructor to hide the implicit public one.
-     */
-    public  FileUtility() {
-//        throw new IllegalArgumentException("Should not acceess this");
-    }
+        /**
+         * Current directory path.
+         */
+        private static final String CURRENT_DIR = System.getProperty("user.dir");
 
-    /**
-     * Writes the Calculation object to a file.
-     *
-    //   * @param budget The Calculation object to save
-     * @throws IOException If an input or output exception occurred
-     */
-    public void writeFile(Budgets budgets) throws IOException {
-        ArrayList<Calculation> calculationList = budgets.retrieveBudgets();
-        File file = new File(FILE_PATH);
+        /**
+         * File path for serialization.
+         */
+        private static final String FILE_PATH = CURRENT_DIR
+                + "/../utility/src/main/resources/budget/utility/savedBudget.json";
 
-        ArrayList<Calculation> existingCalculations;
-
-        // Read existing data if any, or create an empty list
-        if (file.exists()) {
-            existingCalculations = Json.getMapper().readValue(file, Json.getMapper().getTypeFactory()
-                    .constructCollectionType(ArrayList.class, Calculation.class));
-        } else {
-            existingCalculations = new ArrayList<>();
+        /**
+         * Private constructor to hide the implicit public one.
+         */
+        private FileUtility() {
+            throw new IllegalStateException("Utility class");
         }
 
-        existingCalculations.addAll(calculationList);
+        /**
+         * Writes the Calculation object to a file.
+         *
+         * @param calculationMap The map of calculation object to save
+         * @throws IOException If an input or output exception occurred
+         */
+        public static void writeToFile(Map<String, Calculation> calculationMap) throws IOException {
+            File file = new File(FILE_PATH);
+            Json.getMapper()
+                    .writerWithDefaultPrettyPrinter().writeValue(file, calculationMap);
 
-        Json.getMapper().writerWithDefaultPrettyPrinter().writeValue(file, existingCalculations);
+        }
 
+        /**
+         * Sets the load status.
+         *
+         * @param loadStatus The new load status
+         */
+        public static void setLoad(final boolean loadStatus) {
+            load = loadStatus;
+        }
+
+        /**
+         * Reads the Calculation object from a file.
+         *
+         * @param calculationMap The map of calculations to update
+         * @throws IOException If an input or output exception occurred
+         */
+        public static void readFromFile(Map<String, Calculation> calculationMap) throws IOException {
+         Map<String, Calculation> mapFromFile = Json.
+                    getMapper().
+                 readValue(new File(FILE_PATH), new TypeReference<Map<String, Calculation>>() {});
+
+            calculationMap.clear();
+
+            for (Map.Entry<String, Calculation> entry : mapFromFile.entrySet()) {
+                String name = entry.getKey();
+                Calculation calcObject = entry.getValue();
+                calculationMap.put(name, calcObject);
+            }
+        }
+
+        /**
+         * Gets the load status.
+         *
+         * @return The load status
+         */
+        public static boolean getLoad() {
+            return load;
+        }
     }
-    /**
-     * Sets the load status.
-     *
-     * @param loadStatus The new load status
-     */
-    public  void setLoad(boolean loadStatus) {
-        this.load = loadStatus;
-    }
-
-    /**
-     * Reads the Calculation object from a file.
-     * @throws IOException If an input or output exception occurred
-     */
-      public Budgets readFile() throws IOException {
-          File file = new File(FILE_PATH);
-          if (file.exists() && file.length() > 0) {
-              ArrayList<Calculation> calculations = Json.getMapper().readValue(file,
-                      Json.getMapper().getTypeFactory()
-                              .constructCollectionType(ArrayList.class, Calculation.class));
-
-              return new Budgets(calculations);
-          } else {
-              return new Budgets(new ArrayList<>());
-          }
-      }
-    public boolean getLoad() {
-        return this.load;
-    }
-}
