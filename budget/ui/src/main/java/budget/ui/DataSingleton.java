1d40615be46d7f46b3617854b88dabc43a7a7b12
Christopher Høe <christopher.hoee@gmail.com>
2023-10-21 18:19:47 +0200

    feat: Correcly fixed all transitions between controllers/scenes
    Made a new class to store data passed between scenes

--- budget/ui/src/main/java/budget/ui/DataSingleton.java
+++ budget/ui/src/main/java/budget/ui/DataSingleton.java
@@ -2,18 +2,12 @@ package budget.ui;
 
 import budget.core.Calculation;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
 public class DataSingleton {
     private static final DataSingleton instance = new DataSingleton();
 
     private Calculation calculation;
 
     private String calcName;
-
-    private Map<String, Calculation> mapOfCalculations = new HashMap<>();
 
 
     private  DataSingleton(){};
@@ -26,7 +20,7 @@ public class DataSingleton {
     }
 
     public Calculation getCalculation() {
-        return this.calculation;
+        return calculation;
     }
 
     public void setCalcName(String calcName){
@@ -35,15 +29,5 @@ public class DataSingleton {
 
     public String getCalcName(){
         return this.calcName;
-    }
-
-    public void addCalculation(String name, Calculation calc) {
-        this.mapOfCalculations.put(name, calc);
-    }
-    public Map<String, Calculation> getCalculations() {
-        return new HashMap<>(this.mapOfCalculations);
-    }
-    public void updateMap(Map<String, Calculation> tempMap) {
-        this.mapOfCalculations.putAll(tempMap);
     }
 }
