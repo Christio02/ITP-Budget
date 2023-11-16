ce90205ef1d03eea71713edb50ca782b42eed2b6
Mats <mats.kvanvik@hotmail.com>
2023-11-16 11:10:56 +0100

    feat #53: add jlink and jpackage, with documentation
    Co-Authored-by: Christgh (christgh@stud.ntnu.no)

--- budget/ui/src/main/java/module-info.java
+++ budget/ui/src/main/java/module-info.java
@@ -6,7 +6,7 @@ module budget.ui {
     requires com.fasterxml.jackson.databind;
     requires java.net.http;
 
-    opens budget.ui to javafx.fxml, javafx.graphics, javafx.controls, com.fasterxml.jackson.databind, budget.utility;
+    opens budget.ui to javafx.fxml, javafx.graphics, javafx.controls, com.fasterxml.jackson.databind;
 
 
 }
