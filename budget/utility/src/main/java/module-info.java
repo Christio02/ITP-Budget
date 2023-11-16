c7b94750c1ea847519b34175066ede2b92728490
Christopher Høe <christopher.hoee@gmail.com>
2023-11-16 09:46:00 +0100

    fix: removed spring core from utility module

--- budget/utility/src/main/java/module-info.java
+++ budget/utility/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module budget.utility {
     requires budget.core;
     requires com.fasterxml.jackson.databind;
-    requires spring.core;
 
 
     opens budget.utility to com.fasterxml.jackson.databind, budget.ui;
